589dd87518cca01672c6de5deb285fc61b5cb071
GiulioRambelli <giulio.rambelli@eng.it>
2024-12-12 12:51:09 +0100

    This is the first commit.

---
+++ src/test/java/com/giulioram/gestionale/GestionaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giulioram.gestionale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
