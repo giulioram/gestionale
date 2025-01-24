75739df8a88781916741b48d6a3a1a3051ac620c
GiulioRambelli <giulio.rambelli@eng.it>
2025-01-24 15:56:01 +0100

    Add a Github Actions workflow to build and test every pull request.

--- src/test/java/com/giulioram/gestionale/event/EventControllerIntegrationTest.java
+++ src/test/java/com/giulioram/gestionale/event/EventControllerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -56,6 +57,7 @@ public class EventControllerIntegrationTest {
     }
 
     @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     void testFindAllEventsSuccess() throws Exception {
         this.mockMvc.perform(get(this.baseUrl + "/events").accept(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.flag").value(true))
